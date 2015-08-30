0519ce5ec99ba6da67789d3f019338a9004ebaf5
rceraline <r.ceraline@gmail.com>
2015-08-29 20:40:35 -0400

    +HomeCinema.Web project

---
+++ HomeCinema.Web/App_Start/AutoFacWebApiConfig.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using HomeCinema.Data;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Services;
+
+namespace HomeCinema.Web
+{
+    public class AutoFacWebApiConfig
+    {
+        public static IContainer Container { get; set; }
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<HomeCinemaContext>()
+                .As<DbContext>()
+                .InstancePerRequest();
+
+            builder.RegisterType<DbFactory>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>()
+                .InstancePerRequest();
+
+            builder.RegisterGeneric(typeof (EntityRepository<>))
+                .As(typeof (IEntityRepository<>))
+                .InstancePerRequest();
+
+            builder.RegisterType<EncryptionService>()
+                .As<IEncryptionService>()
+                .InstancePerRequest();
+
+            builder.RegisterType<MembershipService>()
+                .As<IMembershipService>()
+                .InstancePerRequest();
+
+            Container = builder.Build();
+            return Container;
+        }
+    }
+}
